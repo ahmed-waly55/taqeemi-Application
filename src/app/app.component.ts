1be4e46106c862e85bbd052c09eb9377addf25cc
ahmed-waly55 <ahmedwaly074@gmail.com>
2026-03-28 15:47:07 +0200

    added navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
+import { FooterComponent } from './shared/components/footer/footer.component';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { app } from '../../server';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent , FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
