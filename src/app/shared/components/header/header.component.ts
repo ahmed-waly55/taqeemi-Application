62e06ce039033b179dabce2ac48af8fc04f7ee33
ahmed-waly55 <ahmedwaly074@gmail.com>
2026-03-28 17:11:43 +0200

    enhanace navbar

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+islogin:boolean = false;
 }
