50c8585ae01dfe1c5698b620478309152057baf9
ahmed-waly55 <ahmedwaly074@gmail.com>
2026-03-29 14:04:41 +0200

    fix bug ssr

--- src/app/layout/landing/landing.component.ts
+++ src/app/layout/landing/landing.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { DecimalPipe } from '@angular/common';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common'
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [],
+  imports: [DecimalPipe],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
-export class LandingComponent {
+export class LandingComponent implements OnInit {
+studentSatisfaction = 0;
+solvedExams = 0;
 
+constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+
+}
+  ngOnInit() {
+   if (isPlatformBrowser(this.platformId)) {
+      this.animateCount('studentSatisfaction', 98, 2000);
+      this.animateCount('solvedExams', 2000000, 2500);
+    }
+  }
+
+  animateCount(property: 'studentSatisfaction' | 'solvedExams', target: number, duration: number) {
+    let startTimestamp: number | null = null;
+    const step = (timestamp: number) => {
+      if (!startTimestamp) startTimestamp = timestamp;
+      const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+      this[property] = Math.floor(progress * target);
+      if (progress < 1) {
+        window.requestAnimationFrame(step);
+      }
+    };
+    window.requestAnimationFrame(step);
+  }
 }
